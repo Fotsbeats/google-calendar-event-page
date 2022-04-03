b9763709c704bc918b68f12ef21352f0f054ff28
Fotsbeats <97646689+Fotsbeats@users.noreply.github.com>
2022-04-03 19:26:53 -0400

    links

--- main.js
+++ main.js
@@ -3,48 +3,48 @@ import './style.css'
 const eventContainer = document.querySelector('#events-container');
 const eventAmtToFetch = document.querySelector('#eventAmt');
 
-const getRandomNumBetween = (min, max) => Math.floor(Math.random() * (max-min +1)) + min;
+const getRandomNumBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getMonth = (month) => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][month];
 const getDayOfWeek = (weekday) => ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][weekday]
 const isAM = (hour) => hour < 12;
 const getHour = (hour) => (hour <= 12 ? hour : hour - 12);
 const getMinute = (minute) => (minute === 0 ? '00' : minute);
 
-function processDate(date){
-  const hour = getHour(date.getHours()) === 0 
-    ? false 
+function processDate(date) {
+  const hour = getHour(date.getHours()) === 0
+    ? false
     : getHour(date.getHours());
   const minute = getMinute(date.getMinutes());
-  const timeSuffix = `<small>${isAM(date.getHours()) 
-    ? `AM` 
+  const timeSuffix = `<small>${isAM(date.getHours())
+    ? `AM`
     : `PM`
-  }</small>`
+    }</small>`
   const time = hour && `${hour}:${minute}${timeSuffix}`;
-  
+
   return {
-    month: getMonth(date.getMonth()), 
+    month: getMonth(date.getMonth()),
     weekday: getDayOfWeek(date.getDay()),
     time,
     date: date.getDate(),
   }
 }
 
-function mapEventObject(event){
-  const startDate = event.start.dateTime 
+function mapEventObject(event) {
+  const startDate = event.start.dateTime
     ? processDate(new Date(event.start.dateTime))
     : processDate(new Date(`${event.start.date}T00:00:00`))
-  const endDate = event.end.dateTime 
+  const endDate = event.end.dateTime
     ? processDate(new Date(event.end.dateTime))
     : processDate(new Date(`${event.end.date}T00:00:00`))
   let dateRange;
-  if (startDate.date !== endDate.date){
+  if (startDate.date !== endDate.date) {
     dateRange = `${startDate.month} ${startDate.date}–${endDate.month} ${endDate.date}`
   } else if (!startDate.time) {
-    dateRange  = `${startDate.month} ${startDate.date}`;
+    dateRange = `${startDate.month} ${startDate.date}`;
   } else {
     dateRange = `${startDate.weekday}, ${startDate.time}–${endDate.time}`;
   }
-  
+
   return {
     name: event.summary,
     description: event.description,
@@ -56,23 +56,20 @@ function mapEventObject(event){
   }
 }
 
-function createEvent(e, i){
-  
-  
+function createEvent(e, i) {
+
+
   return `<article>
           
           <div class="card">
           
-            
-              <p class="white text-xs text-center">${e.dateRange} - ${e.name},  ${e.location}</p> 
-              
-          
-            
-          </div>
+       
+              <a href="${e.description}"><p class="white text-xs text-center">${e.dateRange} - ${e.name},  ${e.location}</p></a>
+            </div>
         </article>`
 }
 
-async function loadEvents(max=200){
+async function loadEvents(max = 200) {
   try {
     const endpoint = await fetch(`./.netlify/functions/calFetch?maxResults=${max}`);
     const data = await endpoint.json();
@@ -85,8 +82,8 @@ async function loadEvents(max=200){
 }
 loadEvents();
 
-eventContainer.addEventListener('click', (e) =>{
-  if(e.target.hasAttribute('aria-expanded')){
+eventContainer.addEventListener('click', (e) => {
+  if (e.target.hasAttribute('aria-expanded')) {
     e.target.setAttribute('aria-expanded', e.target.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
     e.target.querySelector('svg').classList.toggle('rotate-180');
     e.target.nextElementSibling.classList.toggle('hidden');
